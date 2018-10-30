59956c5557af3096dc4d5a3098a6ea548f280da7
levelopers <38830527+levelopers@users.noreply.github.com>
2018-10-29 20:23:01 -0400

    Update java/two stack queue.java

---
+++ java/two stack queue.java
@@ -0,0 +1,36 @@
+/* two stacks queue*/
+
+public class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+    
+    public MyQueue() {
+        stack1 = new Stack<Integer>();
+        stack2 = new Stack<Integer>();
+    }
+    
+    private void stack2ToStack1() {
+        while (! stack2.empty()) {
+            stack1.push(stack2.peek());
+            stack2.pop();
+        }
+    }
+	
+    public void push(int number) {
+        stack2.push(number);
+    }
+
+    public int pop() {
+        if (stack1.empty() == true) {
+            this.stack2ToStack1();
+        }
+        return stack1.pop();
+    }
+
+    public int top() {
+        if (stack1.empty() == true) {
+            this.stack2ToStack1();
+        }
+        return stack1.peek();
+    }
+}
